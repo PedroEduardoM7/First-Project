9a8297975cc3c5e657b07ef75548f7abade3a9df
FujimotoXD <pedroeduardomoraiscostacosta@gmail.com>
2026-04-27 22:05:09 -0300

    First Project - Translater
    Finished

--- scripts.js
+++ scripts.js
@@ -1,42 +1,40 @@
+//Fução IA
 function traduzir() { }
 
-let inputTexto = document.querySelector(".input-texto")
-let Traducaotexto = document.querySelector(".traducao")
-let idioma = document.querySelector(".idioma")
+let inputTexto = document.querySelector(".input-texto");
+let Traducaotexto = document.querySelector(".traducao");
+let idioma = document.querySelector(".idioma");
 
 async function traduzir() {
-    console.log(inputTexto.value)
+    console.log(inputTexto.value);
 
-    let endereco = "https://api.mymemory.translated.net/get?q="
-        + inputTexto.value
-        + "&langpair=pt|"
-        + idioma.value
+    let endereco =
+        "https://api.mymemory.translated.net/get?q=" +
+        inputTexto.value +
+        "&langpair=pt|" +
+        idioma.value;
 
-    let resposta = await fetch(endereco)
-    let dados = await resposta.json()
+    let resposta = await fetch(endereco);
+    let dados = await resposta.json();
 
-    Traducaotexto.textContent = dados.responseData.translatedText
-    console.log(dados)
+    Traducaotexto.textContent = dados.responseData.translatedText;
+    console.log(dados);
 }
-
+//Função Traduzir
 function ouvir() {
+    let voz = window.webkitSpeechRecognition;
 
-    
-    let voz = window.webkitSpeechRecognition
+    let reconhecimento = new voz();
 
-    let reconhecimento = new voz()
-
-    reconhecimento.lang = "pt"
+    reconhecimento.lang = "pt";
 
     reconhecimento.onresult = (evento) => {
-        let TextoTranscricao = evento.results[0][0].transcript
-        inputTexto.textContent = TextoTranscricao
-        console.log(TextoTranscricao)
-    }
+        let TextoTranscricao = evento.results[0][0].transcript;
+        inputTexto.textContent = TextoTranscricao;
+        console.log(TextoTranscricao);
+        traduzir()
 
+    };
 
-
-
-
-    reconhecimento.start()
+    reconhecimento.start();
 }
